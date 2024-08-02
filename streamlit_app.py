4a4a1af5479142ae43c5b0a32b716e08d9724613
LeonaRose33 <125284658+LeonaRose33@users.noreply.github.com>
2024-08-02 19:18:49 +0000

    clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ CONNECTION_DB = st.secrets["connectiondb"]
 st.title("🎈 My DB Connection checking app")
 
 with mysql.connector.connect(**CONNECTION_DB) as conn:
-    st.write(conn)
+    #st.write(conn)
     cursor = conn.cursor()
     query = "SELECT * FROM capacities"
     st.write(f'Execute \n{query}')
