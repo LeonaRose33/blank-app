92a2090a64480ad44e28d978b720fd5512b392c9
LeonaRose33 <125284658+LeonaRose33@users.noreply.github.com>
2024-08-02 17:25:45 +0000

    ffs

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,8 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-try: 
-    with st.connection('sql_connection', type='sql') as conn:
+with st.connection('sql_connection', type='sql') as conn:
         query= f""" SELECT * FROM defaultdb """
         res = conn.query(query)
         if res:
             st.write(res)
-except Exception as e:
-    st.write(e)
