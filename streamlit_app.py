e29e4dd87e12da1265f3b68e4307d4a7ae8db8bd
LeonaRose33 <125284658+LeonaRose33@users.noreply.github.com>
2024-08-02 17:54:55 +0000

    l'p';

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-with st.connection('sql_connection', type='sql') as conn:
-        query= f""" SELECT * FROM defaultdb """
-        res = conn.query(query)
-        if res:
-            st.write(res)
+conn = st.connection('sql_connection')
+query= f""" SELECT * FROM defaultdb """
+res = conn.query(query)
+if res:
+    st.dataframe(res)
