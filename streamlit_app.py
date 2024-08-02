9c476652f32bc22c12ebf303937e901f68563994
LeonaRose33 <125284658+LeonaRose33@users.noreply.github.com>
2024-08-02 19:18:25 +0000

    clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,19 @@
 import streamlit as st
 import mysql.connector
 
-connectiondb = st.secrets["connectiondb"]
+CONNECTION_DB = st.secrets["connectiondb"]
 
-with mysql.connector.connect(**connectiondb) as conn:
+st.title("🎈 My DB Connection checking app")
+
+with mysql.connector.connect(**CONNECTION_DB) as conn:
     st.write(conn)
     cursor = conn.cursor()
     query = "SELECT * FROM capacities"
-    print(f'Execute \n{query}')
+    st.write(f'Execute \n{query}')
     cursor.execute(query)
     results = cursor.fetchall()
     if results:
         columns = [col[0] for col in cursor.description]
-        print('Query executed with SUCCESS')      
+        st.success('Query executed with SUCCESS')      
         st.dataframe(results)
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
