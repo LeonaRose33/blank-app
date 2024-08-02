a310cd949a58e03cfaa7c950b3141c9a2faa5322
LeonaRose33 <125284658+LeonaRose33@users.noreply.github.com>
2024-08-02 16:46:27 +0000

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-secret = st.secrets.items()
-st.write(st.secrets["DB_USERNAME"])
-st.write(st.secrets["DB"])
+connection =st.secrets["DB_URI"]
+st.connection(**connection)
