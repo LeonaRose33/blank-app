e391e8bd352c6e50f2a86ca3670ed1990aa38d1d
LeonaRose33 <125284658+LeonaRose33@users.noreply.github.com>
2024-08-02 18:53:41 +0000

    fdx

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import os
+
+
+import streamlit as st
+import pandas as pd
+import mysql.connector
+
+connectiondb = st.secrets['connectiondb']
+
+with mysql.connector.connect(**connectiondb) as conn:
+    cursor = conn.cursor()
+    query = "SELECT capacity FROM capacities"
+    print(f'Execute \n{query}')
+    cursor.execute(query)
+    results = cursor.fetchall()
+    if results:
+        columns = [col[0] for col in cursor.description]
+        print('Query executed with SUCCESS')      
+        st.dataframe(results)
 
 st.title("🎈 My new app")
 st.write(
